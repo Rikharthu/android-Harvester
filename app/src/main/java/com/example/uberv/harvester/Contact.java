f78ae319c76c5a28366f778cb4346c88ae7b4212
Rikharthu <uberviolence@gmail.com>
2017-03-11 17:28:33 +0200

    камень сдвинулся

---
+++ app/src/main/java/com/example/uberv/harvester/Contact.java
@@ -0,0 +1,102 @@
+package com.example.uberv.harvester;
+
+public class Contact {
+    private String clientName;
+    private String primaryOg;
+    private String mcClassification;
+    private String geoArea;
+    private String role;
+    private String person;
+    private String personLink;
+    private String geoUnit;
+    private String countryForRole;
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getPrimaryOg() {
+        return primaryOg;
+    }
+
+    public void setPrimaryOg(String primaryOg) {
+        this.primaryOg = primaryOg;
+    }
+
+    public String getMcClassification() {
+        return mcClassification;
+    }
+
+    public void setMcClassification(String mcClassification) {
+        this.mcClassification = mcClassification;
+    }
+
+    public String getGeoArea() {
+        return geoArea;
+    }
+
+    public void setGeoArea(String geoArea) {
+        this.geoArea = geoArea;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPerson() {
+        return person;
+    }
+
+    public void setPerson(String person) {
+        this.person = person;
+    }
+
+    public String getPersonLink() {
+        return personLink;
+    }
+
+    public void setPersonLink(String personLink) {
+        this.personLink = personLink;
+    }
+
+    public String getGeoUnit() {
+        return geoUnit;
+    }
+
+    public void setGeoUnit(String geoUnit) {
+        this.geoUnit = geoUnit;
+    }
+
+    public String getCountryForRole() {
+        return countryForRole;
+    }
+
+    public void setCountryForRole(String countryForRole) {
+        this.countryForRole = countryForRole;
+    }
+
+    public Contact(String clientName, String primaryOg, String mcClassification, String geoArea, String role, String person, String personLink, String geoUnit, String countryForRole) {
+
+        this.clientName = clientName;
+        this.primaryOg = primaryOg;
+        this.mcClassification = mcClassification;
+        this.geoArea = geoArea;
+        this.role = role;
+        this.person = person;
+        this.personLink = personLink;
+        this.geoUnit = geoUnit;
+        this.countryForRole = countryForRole;
+    }
+
+    public Contact() {
+
+    }
+}
--- app/src/main/java/com/example/uberv/harvester/MainActivity.java
+++ app/src/main/java/com/example/uberv/harvester/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.uberv.harvester;
 
+import android.Manifest;
+import android.content.Context;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,27 +14,89 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import jxl.Workbook;
+import jxl.write.WritableWorkbook;
+
 public class MainActivity extends AppCompatActivity {
+    public static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     private WebView mWeb;
+    private WritableWorkbook mContactsWorkbook;
 
     /* An instance of this class will be registered as a JavaScript interface */
-    class MyJavaScriptInterface
-    {
+    class MyJavaScriptInterface {
+
+        int prevPage=-1;
+
         @JavascriptInterface
         @SuppressWarnings("unused")
-        public void processHTML(String html)
-        {
+        public void processHTML(String html, String url) {
             // process the html as needed by the app
-            // TODO need some delay for javascript to parse encoded data
-            Log.d("test",html);
+            // TODO check if has data
+            // TODO extract current page from url and max pages from data
+            // current page
+            int index = url.indexOf("page=");
+            final int pageNumber = Integer.parseInt(url.substring(index+5));
+            if(prevPage==pageNumber){
+                // skip
+                // TODO check not to duplicate
+                //return;
+            }
+
+            // process html data
+            Log.d("test", html);
+            Document htmlDoc = Jsoup.parse(html);
+            htmlDoc.getAllElements();
+            Elements contactElements = htmlDoc.getElementsByClass("ese-result-row-item-cont");
+            List<Contact> contacts = new ArrayList<>();
+            for(Element contactElement: contactElements){
+                contacts.add(parseContact(contactElement));
+            }
+
+            // save data
+
+            // load next page
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mWeb.loadUrl("https://search.accenture.com/?aid=ctl&k=clientname|st|google||a||cleangr1|st|&page="+(pageNumber+1));
+                }
+            });
+            Log.d(LOG_TAG, "processHTML");
+
+            prevPage=pageNumber;
         }
     }
 
+    public static Contact parseContact(Element contactElement){
+        Contact contact= new Contact();
+        contact.setClientName(contactElement.getElementsByClass("clientname").text());
+        contact.setCountryForRole(contactElement.getElementsByClass("countryforrole").text());
+        contact.setGeoArea(contactElement.getElementsByClass("geoarea").text());
+        contact.setGeoUnit(contactElement.getElementsByClass("geounit").text());
+        contact.setMcClassification(contactElement.getElementsByClass("mcclassification").text());
+        contact.setPerson(contactElement.getElementsByClass("person").text());
+        // TODO implement
+        // contact.setPersonLink(firstContact.getElementsByClass("clientname").text());
+        contact.setPrimaryOg(contactElement.getElementsByClass("primaryog").text());
+        contact.setRole(contactElement.getElementsByClass("role").text());
+        return contact;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+       // mContactsWorkbook = Workbook.createWorkbook(new File(""));
 
         mWeb = new WebView(this);
         setContentView(mWeb);
@@ -44,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
 
         HarvesterWebViewClient client = new HarvesterWebViewClient();
         mWeb.setWebViewClient(client);
-        mWeb.setWebChromeClient(new WebChromeClient(){
+        mWeb.setWebChromeClient(new WebChromeClient() {
             @Override
             public void onProgressChanged(WebView view, int newProgress) {
                 super.onProgressChanged(view, newProgress);
@@ -65,18 +129,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public static class HarvesterWebViewClient extends WebViewClient {
+        public static enum PageType {
+            Authorization, Search
+        }
+
+        public PageType currentPageType = PageType.Authorization;
 
         @Override
         public void onPageFinished(WebView view, String url) {
             super.onPageFinished(view, url);
 
-            /* This call inject JavaScript into the page which just finished loading. */
-            view.loadUrl("javascript:setTimeout(function(){window.HTMLOUT.processHTML('<head>'+document.getElementsByTagName('html')[0].innerHTML+'</head>');},10000)");
-        }
 
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            return handleUrl(view,url);
+            handleUrl(view, url);
+            if (currentPageType == PageType.Search) {
+                /* This call inject JavaScript into the page which just finished loading. */
+                view.loadUrl("javascript:setTimeout(function(){window.HTMLOUT.processHTML('<head>'+document.getElementsByTagName('html')[0].innerHTML+'</head>','"+url+"');},1)");
+            }
         }
 
         @Override
@@ -86,16 +154,25 @@ public class MainActivity extends AppCompatActivity {
 
         // handle url
         private boolean handleUrl(WebView view, String url) {
-            final Uri uri = Uri.parse(url);
+            Uri uri = Uri.parse(url);
 //            Log.i(TAG, "Uri =" + uri);
-            final String host = uri.getHost();
-            final String scheme = uri.getScheme();
+            String host = uri.getHost();
+            String scheme = uri.getScheme();
             // Based on some condition you need to determine if you are going to load the url
             // in your web view itself or in a browser.
             // You can use `host` or `scheme` or any part of the `uri` to decide.
 
             // host: federation-sts.accenture.com
-            view.loadUrl(url);
+            // search.accenture.com
+            switch (host) {
+                case "federation-sts.accenture.com":
+                    currentPageType = PageType.Authorization;
+                    break;
+                case "search.accenture.com":
+                    currentPageType = PageType.Search;
+                    break;
+            }
+            Log.d(LOG_TAG,"handleUrl");
             return true;
         }
     }
